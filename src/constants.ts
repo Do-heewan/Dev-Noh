0c201ae3997adcbb9bdd1de1d1146ee6412e1cef
Noh <nhw3152@gmail.com>
2026-03-17 11:59:03 +0900

    feat: 페이지 그리드 레이아웃 형식 추가

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const koreanTagNames: Record<string, string> = {
+    react: "리액트",
+    nextjs: "Next.js",
+    typescript: "타입스크립트",
+    javascript: "자바스크립트",
+    css: "CSS",
+    tailwind: "테일윈드",
+    astro: "Astro",
+    notion: "노션",
+    backend: "백엔드",
+    frontend: "프론트엔드",
+    devops: "데브옵스",
+    database: "데이터베이스",
+};
